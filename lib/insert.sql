d8f38588a0486555c6b1b5e7904db0a4d381062f
Katie Moosman <moosman.katie@gmail.com>
2019-02-01 04:32:33 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate", 9, "male","brown","moody","yes");
+INSERT (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate", 9, "male","brown","moody","yes");
+INSERT (name, age, gender, color, temperment, alive) VALUES ("Rowdy", 3, "male","green","cuddly","yes");
+INSERT (name, age, gender, color, temperment, alive) VALUES ("Tabitha", 15, "female","brown","moody","no");
+INSERT (name, age, gender, color, temperment, alive) VALUES ("Sergeant Brown", 3, "male","black","happy","yes");
+INSERT (name, age, gender, color, temperment, alive) VALUES ("Melissa", 3, "female","brown","happy","yes");
+INSERT (name, age, gender, color, temperment, alive) VALUES ("Grinch", 20, "male","brown","bitter","no");
+INSERT (name, age, gender, color, temperment, alive) VALUES ("Wendy", 3, "female","pink","sad","yes");
+INSERT (name, age, gender, color, temperment, alive) VALUES (NULL, 3, "female","brown","happy","yes");
